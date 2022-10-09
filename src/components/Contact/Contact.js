1bff9f1dad5cd2a668e4d6a8db653d102670e8ec
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-09 20:21:41 +0600

    added favicon and Contact Us page headline

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-
-        </div>
+        <section className='mt-12 mb-20'>
+            <div className='flex justify-center mb-24'>
+                <div className='text-center md:w-4/5 md:m-0 mx-2'>
+                    <h1 className='bg-gray-400 text-4xl text-white font-bold rounded-lg w-full mb-4 p-10'>Contact Us</h1>
+                </div>
+            </div>
+        </section>
     );
 };
 
