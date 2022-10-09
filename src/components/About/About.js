786236bf62f3eba00fee971d942437c7be7f1a88
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-09 20:13:34 +0600

    completed About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-
-        </div>
+        <section className='mt-12 mb-20'>
+            <div className='flex justify-center mb-24'>
+                <div className='text-center md:w-4/5 md:m-0 mx-2'>
+                    <h1 className='bg-gray-400 text-4xl text-white font-bold rounded-lg w-full mb-4 p-10'>About Us</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi similique reiciendis dolorem quas in fugiat aspernatur labore, veniam dolor illum sed ipsam molestiae impedit totam alias eos eum nemo. Tempore, quae a atque ipsa, sequi laborum magni libero dolorem, sit quis enim? Dolorem fugit hic, officiis dignissimos voluptatem voluptate vitae excepturi amet accusamus voluptatibus omnis qui rerum laudantium asperiores. Nam sunt doloremque dolores laudantium ipsam dolorum cum id, aperiam iste accusamus beatae vel, nobis itaque hic porro delectus accusantium quo animi eos voluptate sed recusandae exercitationem nihil. Consequuntur, deserunt magnam. Quidem porro provident maxime temporibus eos voluptatem ad, quod illum.ipsam molestiae impedit totam alias eos eum nemo. Tempore, quae a atque ipsa, sequi laborum magni libero dolorem, sit quis enim? Dolorem fugit hic, officiis dignissimos voluptatem voluptate vitae excepturi amet accusamus voluptatibus omnis qui rerum laudantium asperiores. Nam sunt doloremque dolores laudantium ipsam dolorum cum id, aperiam iste accusamus beatae vel, nobis itaque hic porro delectus accusantium quo animi eos voluptate sed recusandae exercitationem nihil. Consequuntur, deserunt magnam. Quidem porro provident maxime temporibus eos voluptatem ad, quod illum.</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='bg-indigo-600 text-center text-white w-full fixed bottom-0 px-5 py-3'>
-            <p>&copy; 2022 - Developed by <a className='text-red-700 font-semibold' href="https://www.linkedin.com/in/ehsanulislamkhan/" target="_blank" rel="noopener noreferrer">Ehsan</a></p>
+            <p>&copy; 2022 - Developed by <a className='text-yellow-400 font-semibold' href="https://www.linkedin.com/in/ehsanulislamkhan/" target="_blank" rel="noopener noreferrer">Ehsan</a></p>
         </footer>
     );
 };
