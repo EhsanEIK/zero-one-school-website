bd07b80804dbc0826971030a706057a58cb3c740
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-10 09:42:30 +0600

    added axios and get chart data by using axios

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,29 +1,44 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Chart = () => {
     const [data, setData] = useState([]);
     useEffect(() => {
-        fetch('https://raw.githubusercontent.com/ProgrammingHero1/product-analysis-website/main/data.json')
-            .then(res => res.json())
-            .then(data => setData(data));
+        // fetch('https://raw.githubusercontent.com/ProgrammingHero1/product-analysis-website/main/data.json')
+        //     .then(res => res.json())
+        //     .then(data => setData(data));
+        axios.get('https://raw.githubusercontent.com/ProgrammingHero1/product-analysis-website/main/data.json')
+            .then(data => {
+                let year = 1990;
+                const allData = data.data;
+                const newData = allData.map(d => {
+                    d.marks = d.sell;
+                    d.year = (year = year + 5);
+                    delete d.revenue;
+                    return d;
+                })
+                setData(newData)
+            })
     }, []);
 
     return (
-        <section className='flex justify-center mt-20'>
-            <LineChart
-                width={800}
-                height={400}
-                data={data}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
-            </LineChart>
+        <section className='my-40'>
+            <h1 className='text-4xl text-center font-bold mb-20'>Progress Chart</h1>
+            <div className='flex justify-center'>
+                <LineChart
+                    width={800}
+                    height={400}
+                    data={data}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="year" />
+                    <YAxis dataKey="marks" />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="marks" stroke="#8884d8" activeDot={{ r: 8 }} />
+                </LineChart>
+            </div>
         </section>
     );
 };
