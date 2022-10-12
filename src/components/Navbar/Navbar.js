dd7a16091983ae8ec46912e8aac2899c05e5f89e
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-12 14:20:32 +0600

    added animation in navbar toggle

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,14 +6,14 @@ const Navbar = () => {
     const [open, setOpen] = useState(false);
 
     return (
-        <nav className='bg-indigo-600 text-white md:flex md:justify-between items-center py-4'>
+        <nav className='bg-indigo-600 text-white flex justify-between items-center md:py-4 pt-4 pb-12'>
             <div onClick={() => setOpen(!open)} className="h-8 w-8 text-white-500 md:hidden ml-5">
                 {
                     open ? <XMarkIcon /> : <Bars3Icon />
                 }
             </div>
-            <h1 className='text-5xl font-extrabold text-yellow-500 text-center md:ml-14 md:mb-0 mb-5'>ZERO<span className='text-red-600'>1</span></h1>
-            <div className={`md:block md:ml-0 ml-5 ${open ? 'block' : 'hidden'}`}>
+            <h1 className='md:text-5xl text-3xl font-extrabold text-yellow-500  md:ml-14 md:mr-0 mr-5'>ZERO<span className='text-red-600'>1</span></h1>
+            <div className={`md:static md:ml-0 ml-5 md:mt-0 mt-20 absolute duration-500 ease-in-out ${open ? 'left-2' : '-left-96'}`}>
                 <NavLink className={({ isActive }) => isActive ? 'bg-red-600 md:mr-3 mr-0 px-2 py-1' : 'md:mr-3 mr-0 px-2 py-1'} to={'/home'}>Home</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'bg-red-600 md:mr-3 mr-0 px-2 py-1' : 'md:mr-3 mr-0 px-2 py-1'} to={'/courses'}>Courses</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'bg-red-600 md:mr-3 mr-0 px-2 py-1' : 'md:mr-3 mr-0 px-2 py-1'} to={'/instructors'}>Instructor</NavLink>
